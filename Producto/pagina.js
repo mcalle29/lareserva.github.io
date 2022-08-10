70184a381b49d18e52cdcbd2ef0e57a0fa43ae02
La reserva <74299622@sanmartin.coar.edu.pe>
2022-08-09 20:42:59 -0500

    Add files via upload

---
+++ Producto/pagina.js
@@ -0,0 +1,167 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+var swiper = new Swiper(".food-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".food-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".menu-slider", {
+  grabCursor:true,
+  loop:true,
+  autoHeight:true,
+  centeredSlides:true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let previewContainer = document.querySelector('.food-preview-container');
+let previewBox = previewContainer.querySelectorAll('.food-preview');
+
+document.querySelectorAll('.food .slide').forEach(food =>{
+  food.onclick = () =>{
+    previewContainer.style.display = 'flex';
+    let name = food.getAttribute('data-name');
+    previewBox.forEach(preveiw =>{
+        let target = preveiw.getAttribute('data-target');
+        if(name == target){
+          preveiw.classList.add('active');
+        }
+      });
+    };
+  });
+  previewContainer.querySelector('#close-preview').onclick = () =>{
+  previewContainer.style.display = 'none';
+  previewBox.forEach(close =>{
+    close.classList.remove('active');
+  });
+};
+$(document).ready(function(){
+
+  $('#menu-bar').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('load scroll',function(){
+
+      $('#menu-bar').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+
+      $('section').each(function(){
+
+          let top = $(window).scrollTop();
+          let height = $(this).height();
+          let id = $(this).attr('id');
+          let offset = $(this).offset().top - 200;
+
+          if(top > offset && top < offset + height){
+              $('.navbar ul li a').removeClass('active');
+              $('.navbar').find(`[href="#${id}"]`).addClass('active');
+          }
+
+      });
+
+  });
+
+  $('.list .btn').click(function(){
+      $(this).addClass('active').siblings().removeClass('active');
+      let src = $(this).attr('data-src');
+      $('.menu .row .image img').attr('src',src);
+  });
+
+});
